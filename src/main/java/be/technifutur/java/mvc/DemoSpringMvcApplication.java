a752e5866d516f1a7db456c54f1a3ee764860240
Charlotte Bartholomé <charlotte.bartholome@gmail.com>
2023-01-25 08:42:10 +0100

    V1

--- src/main/java/be/technifutur/java/mvc/DemoSpringMvcApplication.java
+++ src/main/java/be/technifutur/java/mvc/DemoSpringMvcApplication.java
@@ -1,7 +1,9 @@
 package be.technifutur.java.mvc;
 
+import be.technifutur.java.mvc.utils.EMFSharer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Scanner;
 
@@ -9,10 +11,10 @@ import java.util.Scanner;
 public class DemoSpringMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringMvcApplication.class, args);
+		ApplicationContext ctxt = SpringApplication.run(DemoSpringMvcApplication.class, args);
 
 
-
+		ctxt.getBean(EMFSharer.class).close();
 	}
 
 }
